20b61a6654f503d0edfeeeb009f1332e398cf5e5
Ian <iansedano@gmail.com>
2021-05-17 18:11:10 +0200

    factoring out productContainer

--- src/Products.js
+++ src/Products.js
@@ -1,71 +1,16 @@
-import React from 'react';
+import React from "react";
+import { ProductContainer } from "./ProductContainer";
 
-function ProductDescription({description}){
+export function ProductGrid({ productList }) {
   const style = {
-    "overflow": "hidden",
-    "display": "-webkit-box",
-    "-webkit-line-clamp": "3",
-    "-webkit-box-orient": "vertical"
-  }
-  return(
-    <p style={style}>{description}</p>
-  )
-}
-
-function ProductTitle({ title }){
-  const containerStyle = {
-    "height": "11em",
-    "width": "90%",
-    "margin": "5%",
-    "display": "flex",
-    "justifyContent": "center",
-    "alignItems": "center"
-  }
-
-  const headerStyle = {
-  }
-
-  return <div style={containerStyle}>
-      <h2 style={headerStyle}>{title}</h2>
-    </div>
-}
-
-function ProductContainer({ product }){
-  const style = {
-    "margin": "20px",
-    "background": "grey",
-    "textAlign": "center",
-    "width": "300px",
-    "height": "500px",
-  }
+    display: "flex",
+    flexWrap: "wrap",
+  };
   return (
     <div style={style}>
-      <ProductTitle title={product.title}/>
-      <ProductDescription description = {product.description}/>
-      <img
-        src={product.image}
-        style={{
-          maxHeight:"150px",
-          maxWidth:"100%",
-          objectFit: "contain"
-        }}>
-      </img>
+      {productList.map((product, i) => {
+        return <ProductContainer key={i} product={product} />;
+      })}
     </div>
-  )
-}
-
-export function ProductGrid({productList}){
-  const style = {
-    "display": "flex",
-    "flexWrap": "wrap"
-  }
-  return (
-    <div style={style}>
-      {
-        productList.map((product, i) => {
-          return <ProductContainer key={i} product = {product}/>
-        })
-      }
-    </div>
-  )
+  );
 }
